11fcffd9ed6ebf2eb5d3efe694a7bf14330f77cb
Benoît AUGER--DUBOIS <E188469H@univ-nantes.fr>
2023-03-23 10:14:07 +0100

    fix: remove history file

--- .history/src/components/User_20230317150333.jsx
+++
@@ -1,9 +0,0 @@
-export default function User (props) {
-    console.log(props)
-    return(
-        <div>
-            <h3>{props.user.firstName}</h3>
-            <h4>{props.user.lastName}</h4>
-        </div>
-    )
-}
