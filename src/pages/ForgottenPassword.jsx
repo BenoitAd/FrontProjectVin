e500e0ccadb285aae1ac415eabeb51f30ee90e7e
Benoît AUGER--DUBOIS <E188469H@univ-nantes.fr>
2023-03-23 13:32:18 +0100

    feat : style forgottent password

--- src/pages/ForgottenPassword.jsx
+++ src/pages/ForgottenPassword.jsx
@@ -1,64 +1,75 @@
+import Navbar from "../components/Navbar";
+
 export default function ForgottenPassword() {
-    return (
-    <div class="font-mono bg-gray-400">
-		<div class="container mx-auto">
-			<div class="flex justify-center px-6 my-12">
-				<div class="w-full xl:w-3/4 lg:w-11/12 flex">
-                <img
-                    src="https://cdn.aveine.paris/blog/wp-content/uploads/2021/09/27102902/shutterstock_619382510.jpg"
-                    class="w-full h-auto bg-gray-400 hidden lg:block lg:w-1/2 bg-cover rounded-l-lg"
-                    alt="Sample"
-                    />
-					<div class="w-full lg:w-1/2 bg-white p-5 rounded-lg lg:rounded-l-none">
-						<div class="px-8 mb-4 text-center">
-							<h3 class="pt-4 mb-2 text-2xl">Forgot Your Password?</h3>
-							<p class="mb-4 text-sm text-gray-700">
-								We get it, stuff happens. Just enter your email address below and we'll send you a
-								link to reset your password!
-							</p>
-						</div>
-						<form class="px-8 pt-6 pb-8 mb-4 bg-white rounded">
-							<div class="mb-4">
-								<label class="block mb-2 text-sm font-bold text-gray-700" for="email">
-									Email
-								</label>
-								<input
-									class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-									id="email"
-									type="email"
-									placeholder="Enter Email Address..."
-								/>
-							</div>
-							<div class="mb-6 text-center">
-								<button
-									class="w-full px-4 py-2 font-bold text-white bg-red-500 rounded-full hover:bg-red-700 focus:outline-none focus:shadow-outline"
-									type="button"
-								>
-									Reset Password
-								</button>
-							</div>
-							<hr class="mb-6 border-t" />
-							<div class="text-center">
-								<a
-									class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
-									href="./register.html"
-								>
-									Create an Account!
-								</a>
-							</div>
-							<div class="text-center">
-								<a
-									class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
-									href="./index.html"
-								>
-									Already have an account? Login!
-								</a>
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-    )
+  return (
+    <section className="h-screen">
+      <div class="h-full text-gray-800">
+        <div class="grid grid-cols-2 w-full h-full g-6">
+          <div class="col-span-1 mb-12 md:mb-0 h-full inset-0">
+            <img
+              src="https://cdn.aveine.paris/blog/wp-content/uploads/2021/09/27102902/shutterstock_619382510.jpg"
+              class="w-full h-full object-cover object-left ml-0"
+              alt="Sample"
+            />
+          </div>
+          <div class="col-span-1 w-full ">
+            <div>
+              <Navbar />
+            </div>
+            <div class="mt-24 mx-auto my-10 flex items-center w-1/2 justify-center flex-col">
+              <div class="px-8 mb-4 text-center">
+                <h3 class="pt-4 mb-2 text-4xl font-bold">Mot de passe oublié ?</h3>
+                <p class="mb-4 text-xl	 text-gray-700 ">
+                  Ca arrive pas de panique. Entrer l'adresse Email de votre
+                  compte et nous vous enverrons un lien pour modifier votre mot
+                  de passe !
+                </p>
+              </div>
+              <form class="px-8 pt-6 pb-8 mb-4 bg-white rounded">
+                <div class="mb-4">
+                  <label
+                    class="block mb-2 text-xl font-bold text-gray-700"
+                    for="email"
+                  >
+                    Email
+                  </label>
+                  <input
+                    class="w-full px-3 py-2 text-xl leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                    id="email"
+                    type="email"
+                    placeholder="Entrer votre Addresse Email ..."
+                  />
+                </div>
+                <div class="mb-6 text-center">
+                  <button
+                    class="w-full px-4 py-2 font-bold text-white bg-red-500 rounded-full hover:bg-red-700 focus:outline-none focus:shadow-outline"
+                    type="button"
+                  >
+                    Changer de mot de passe
+                  </button>
+                </div>
+                <hr class="mb-6 border-t" />
+                <div class="text-center">
+                  <a
+                    class="inline-block text-xl text-blue-500 align-baseline hover:text-blue-800"
+                    href="./register.html"
+                  >
+                    Creer un compte !
+                  </a>
+                </div>
+                <div class="text-center">
+                  <a
+                    class="inline-block text-xl text-blue-500 align-baseline hover:text-blue-800"
+                    href="./index.html"
+                  >
+                    Vous avez deja un compte ? Connectez-vous!
+                  </a>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
